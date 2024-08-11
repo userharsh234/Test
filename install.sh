c321a006057a1ff1b4733245f3664c9dc28e5776
userharsh234 <Pradoshmishra28850@gmail.com>
2024-08-11 22:38:15 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cat <<EOL > .devcontainer/devcontainer.json
 {
     "name": "My Codespace",
     "image": "mcr.microsoft.com/vscode/devcontainers/python:3.8",
-    "postStartCommand": "python3 /workspaces/bgmi/m.py",
+    "postStartCommand": "python3 /workspaces/Test/d.py",
     "customizations": {
         "vscode": {
             "settings": {
